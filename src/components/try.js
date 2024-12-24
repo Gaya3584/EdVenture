4c5037ce5c780e454a4d197efb0314ca0611efcc
Gaya3584 <gayugayus584@gmail.com>
2024-12-24 20:54:58 +0530

    design completed

--- src/components/try.js
+++ src/components/try.js
@@ -10,7 +10,7 @@ const Try = () => {
         {/* Lectures */}
         <div className="activity-division lectures">
           <h3>Lectures</h3>
-          <p>Watch and understand concepts through engaging video lessons.</p>
+          <p className="actpara">Watch and understand concepts through engaging video lessons.</p>
           <div className="activity-list">
             <div className="activity-item">
               <img src="https://via.placeholder.com/200x150" alt="Lecture 1" />
@@ -26,7 +26,7 @@ const Try = () => {
         {/* Fun Activities */}
         <div className="activity-division fun-activities">
           <h3>Fun Activities</h3>
-          <p>Learn concepts through games and exciting activities.</p>
+          <p className="actpara">Learn concepts through games and exciting activities.</p>
           <div className="activity-list">
             <div className="activity-item">
               <img src="https://via.placeholder.com/200x150" alt="Game 1" />
